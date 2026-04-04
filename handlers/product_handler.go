a5ee7526bec4ce4a34b9e999cb621cdc2684ff0f
Nurca <galnur2022@gmail.com>
2026-04-04 11:07:12 +0500

    delete comment

--- handlers/product_handler.go
+++ handlers/product_handler.go
@@ -53,19 +53,16 @@ func UpdateProduct(c *gin.Context) {
 	id := c.Param("id")
 	var product models.Product
 
-	// проверяем есть ли продукт
 	if err := config.DB.First(&product, id).Error; err != nil {
 		c.JSON(http.StatusNotFound, gin.H{"error": "Product not found"})
 		return
 	}
 
-	// принимаем новые данные
 	if err := c.ShouldBindJSON(&product); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input"})
 		return
 	}
 
-	// обновляем
 	if err := config.DB.Save(&product).Error; err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update product"})
 		return
